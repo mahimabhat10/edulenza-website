b770f08b4161a241b307b8609f23ba3596f8ded2
Mahima Bhat <mahimabhat2@gmail.com>
2026-06-11 03:59:52 +0530

    Homepage skeleton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
 function Footer() {
-  return <h2>Footer</h2>;
+  return (
+    <footer>
+      <h3>Edulenza</h3>
+
+      <p>
+        AI-powered microlearning and career development platform.
+      </p>
+
+      <p>support@edulenza.com</p>
+
+      <p>© 2026 Edulenza</p>
+    </footer>
+  );
 }
 
 export default Footer;
